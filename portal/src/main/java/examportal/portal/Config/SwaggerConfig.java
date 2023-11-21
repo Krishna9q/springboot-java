03f56618d626b923ddd4f9edd804626431619589
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2023-11-21 15:32:11 +0530

    try 7

--- portal/src/main/java/examportal/portal/Config/SwaggerConfig.java
+++ portal/src/main/java/examportal/portal/Config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package examportal.portal.Config;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -12,38 +15,46 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
-
 @Configuration
 public class SwaggerConfig {
 
-    Logger log = LoggerFactory.getLogger("SwaggerConfig");
-      @Bean
-    public OpenAPI openAPI(){
-        log.info("SwaggerConfig ,openAPI Method Start");
-        String schemeName ="ProExaminator";
-        log.info("SwaggerConfig ,openAPI Method Ends");
-        return new OpenAPI()
-        .addSecurityItem(new SecurityRequirement()
-        .addList(schemeName)
-        )
-        .components(new Components()
-        .addSecuritySchemes(schemeName, new SecurityScheme()
-        .name(schemeName)
-        .type(SecurityScheme.Type.HTTP)
-        .bearerFormat("JWT")
-        .scheme("bearer")
-        
+  Logger log = LoggerFactory.getLogger("SwaggerConfig");
 
-        )
-        )
-        .info(new Info()
-        .title("Exam-Portal Documentation")
-        .description(("This Is Exam-Portal Application"))
-        .version("1.0")
-        .contact(new Contact().name("Krishnas.bca2022@ssism.org").email("Krishnas@ssism.org").url("WarehouseManagement.com"))
-        .license(new License().name("Apache"))
-        ).externalDocs(new ExternalDocumentation().url("ExamPortal_.com").description("This is external url"));
-        
-    }
+  @Bean
+  public OpenAPI openAPI() {
+    log.info("SwaggerConfig, openAPI Method Start");
     
+    String schemeName = "ProExaminator";
+    log.info("SwaggerConfig, openAPI Method Ends");
+    return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement().addList(schemeName))
+        .components(new Components()
+            .addSecuritySchemes(schemeName, new SecurityScheme()
+                .name(schemeName)
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer")))
+        .info(new Info()
+            .title("Exam-Portal Documentation")
+            .description(("This Is Exam-Portal Application"))
+            .version("1.0")
+            .contact(new Contact().name("Krishnas.bca2022@ssism.org").email("Krishnas@ssism.org")
+                .url("WarehouseManagement.com"))
+            .license(new License().name("Apache")))
+        .externalDocs(new ExternalDocumentation().url("ExamPortal_.com").description("This is external url"));
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
+  }
 }
