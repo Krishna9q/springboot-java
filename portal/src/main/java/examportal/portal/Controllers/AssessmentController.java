a74a966c0936b10e9d3d90b4527f77a38194267d
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2023-11-24 13:49:02 +0530

    try 16

--- portal/src/main/java/examportal/portal/Controllers/AssessmentController.java
+++ portal/src/main/java/examportal/portal/Controllers/AssessmentController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ import examportal.portal.Repo.AssessmentRepo;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+// @CrossOrigin(origins = "*")
 public class AssessmentController {
 
     @Autowired
