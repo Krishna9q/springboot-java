469a6b4251233436000e2ae05f4a45464797fcb3
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2023-12-05 16:51:27 +0530

    new try 20

--- portal/src/main/java/examportal/portal/Services/UserService.java
+++ portal/src/main/java/examportal/portal/Services/UserService.java
@@ -2,13 +2,10 @@ package examportal.portal.Services;
 
 import java.util.List;
 import examportal.portal.Entity.User;
-import examportal.portal.Payloads.userDto;
 
 public  interface UserService {
     
-    User createUser(userDto user);
-    
-    String sendmail(User user);
+    User createUser(User user);
     
     List<User>getAllUser();
 
