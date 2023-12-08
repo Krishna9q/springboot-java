12cd56fa70c34622f419e78a397cd04b702e3083
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2023-12-08 11:53:55 +0530

    83

--- portal/src/main/java/examportal/portal/Controllers/ResultController.java
+++ portal/src/main/java/examportal/portal/Controllers/ResultController.java
@@ -1,4 +1,5 @@
 package examportal.portal.Controllers;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import examportal.portal.Entity.Cheating;
+import examportal.portal.Entity.Result;
 import examportal.portal.Entity.Student;
 import examportal.portal.Payloads.ResultDto;
 import examportal.portal.Payloads.checkpaperDto;
 import examportal.portal.Repo.CheatingRepo;
+import examportal.portal.Repo.ResultRepo;
 import examportal.portal.Services.ResultService;
 
 @RestController
@@ -21,6 +25,10 @@ public class ResultController {
 
     @Autowired
     private ResultService resultService;
+
+    
+    @Autowired
+    private ResultRepo resultRepo;
 
     @Autowired
     private CheatingRepo cheatingRepo;
@@ -33,10 +41,10 @@ public class ResultController {
         return new ResponseEntity<>(result,HttpStatus.CREATED);
     }
 
-    @GetMapping("/Get/result/{resultID}")
-    public ResponseEntity<ResultDto> getresultByID(@PathVariable String resultID)
+    @GetMapping("/Get/result/{resultId}")
+    public ResponseEntity<ResultDto> getresultByID(@PathVariable String resultId)
     {
-        ResultDto resultDto = this.resultService.getResultByResultId(resultID);
+        ResultDto resultDto = this.resultService.getResultByStudentAndPaperId(resultId);
         return new ResponseEntity<>(resultDto,HttpStatus.OK);
     }
 
@@ -63,4 +71,12 @@ public class ResultController {
         return new ResponseEntity<>(toppers,HttpStatus.OK);
     }
 
+      @GetMapping("/getAllResultsByStudentId/{studentId}")
+    public ResponseEntity <java.util.List<Result>>getAllResult_ByStudentId(@PathVariable String studentId)
+    {
+       
+        java.util.List<Result> results = this.resultRepo.findByStudentID(studentId);
+
+        return new ResponseEntity<>(results,HttpStatus.OK);
+    }
 }
